d2eaa34f391db99fa164479b84656be384c8cee9
Victor Higaki <victorhigaki@outlook.com>
2025-05-27 00:20:35 -0300

    19. Adding the delete auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -79,4 +79,22 @@ public class AuctionsController : ControllerBase
 
         return BadRequest("Problem saving changes");
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+
+        if (auction == null) return NotFound();
+
+        // TODO: check seller == username
+
+        _context.Auctions.Remove(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (!result) return BadRequest("Could not update DB");
+
+        return Ok();
+    }
 }
