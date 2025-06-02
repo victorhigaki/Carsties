dffc226dcd3a5d9325def321e367bac78d9b72fe
Victor Higaki <victorhigaki@outlook.com>
2025-06-02 00:37:11 -0300

    44. Consuming fault queues

--- src/SearchService/Consumers/AuctionCreatedConsumer.cs
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -17,9 +17,11 @@ public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
 
     public async Task Consume(ConsumeContext<AuctionCreated> context)
     {
-        Console.WriteLine("--> Consuming auction crated: " + context.Message.Id);
+        Console.WriteLine("--> Consuming auction created: " + context.Message.Id);
 
         var item = _mapper.Map<Item>(context.Message);
+
+        if (item.Model == "Foo") throw new ArgumentException("Cannot sell cars with name of Foo");
 
         await item.SaveAsync();
     }
