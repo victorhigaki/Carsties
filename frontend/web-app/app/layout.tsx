415d64ee8a79fdd772696b10b1cf6406e2bb7c70
Victor Higaki <victorhigaki@outlook.com>
2025-06-19 08:47:38 -0300

    86. Creating a nav bar

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NavBar from "./nav/NavBar";
 
 export const metadata: Metadata = {
   title: "Carsties",
@@ -14,7 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <NavBar />
+        <main className="container mx-auto px-5 pt-10">
+          {children}
+        </main>
       </body>
     </html>
   );
--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-semibold">Carsties App!</h1>
+      Content goes here
     </div>
   );
 }
