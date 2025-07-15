91ec40976e8ce19b72cf6725b3744184fd983de1
Victor Higaki <victorhigaki@outlook.com>
2025-07-14 22:50:06 -0300

    109. Getting the session details in the client

--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,14 +1,22 @@
+import { getCurrentUser } from "../actions/authActions";
 import LoginButton from "./LoginButton";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserActions from "./UserActions";
 
-export default function NavBar() {
+export default async function NavBar() {
+    const user = await getCurrentUser();
+
     return (
         <header className="sticky top-0 z-50 flex justify-between 
             bg-white p-5 items-center text-gray-800 shadow-md">
             <Logo />
             <Search />
-            <LoginButton />
+            {user ? (
+                <UserActions />
+            ) : (
+                <LoginButton />
+            )}
         </header>
     )
 }
