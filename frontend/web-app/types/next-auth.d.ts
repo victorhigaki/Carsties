523f154aa6c16d4cf6ea06dc5eb7a9a3cbd817ec
Victor Higaki <victorhigaki@outlook.com>
2025-07-16 23:52:35 -0300

    110. Populating the session data

---
+++ frontend/web-app/types/next-auth.d.ts
@@ -0,0 +1,25 @@
+import { type DefaultSession } from "next-auth"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from 'next-auth/jwt'
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            username: string
+        } & DefaultSession["user"]
+    }
+
+    interface Profile {
+        username: string;
+    }
+
+    interface User {
+        username: string;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        username: string;
+    }
+}
