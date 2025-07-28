f434d511be33386f544e5bcad19ad60d54d52a70
Victor Higaki <victorhigaki@outlook.com>
2025-07-28 16:18:29 -0300

    120. Creating an Auction form part 2

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,5 +1,53 @@
+'use client'
+
+import { Button, HelperText, Spinner, TextInput } from "flowbite-react";
+import { useRouter } from "next/navigation";
+import { FieldValues, useForm } from "react-hook-form";
+
 export default function AuctionForm() {
-  return (
-    <div>Auction Form goes there</div>
-  )
+    const router = useRouter();
+    const { register, handleSubmit, setFocus,
+        formState: { isSubmitting, isValid, isDirty, errors } } = useForm();
+
+    function onSubmit(data: FieldValues) {
+        console.log(data);
+    }
+
+    return (
+        <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3 block">
+                <TextInput
+                    {...register('make', { required: 'Make is required' })}
+                    placeholder='Make'
+                    color={errors?.make && 'failure'}
+                />
+                <HelperText color="failure">
+                    {errors.make?.message as string}
+                </HelperText>
+            </div>
+            <div className="mb-3 block">
+                <TextInput
+                    {...register('model', { required: 'Model is required' })}
+                    placeholder='Model'
+                    color={errors?.model && 'failure'}
+                />
+                <HelperText color="failure">
+                    {errors.model?.message as string}
+                </HelperText>
+            </div>
+
+            <div className="flex justify-between">
+                <Button color='alternative' onClick={() => router.push('/')}>Cancel</Button>
+                <Button
+                    outline
+                    color='green'
+                    type="submit"
+                    disabled={!isValid || !isDirty}
+                >
+                    {isSubmitting && <Spinner size="sm" />}
+                    Submit
+                </Button>
+            </div>
+        </form>
+    )
 }
