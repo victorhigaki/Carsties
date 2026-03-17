4f8254181c639ee34814079b64b8af09bc05e2d9
Victor Higaki <victorhigaki@outlook.com>
2026-03-17 07:52:08 -0300

    160. Getting the bids for an auction

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,33 +1,43 @@
-'use server';
+"use server";
 
 import { fetchWrapper } from "@/lib/fetchWrapper";
-import { PagedResult, Auction } from "@/types";
+import { PagedResult, Auction, Bid } from "@/types";
 import { FieldValues } from "react-hook-form";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
-    return fetchWrapper.get(`search${query}`);
+  return fetchWrapper.get(`search${query}`);
 }
 
-export async function updateAuctionTest(): Promise<{status: number, message: string}> {
-    const data = {
-        mileage: Math.floor(Math.random() * 10000) + 1
-    } 
+export async function updateAuctionTest(): Promise<{
+  status: number;
+  message: string;
+}> {
+  const data = {
+    mileage: Math.floor(Math.random() * 10000) + 1,
+  };
 
-    return fetchWrapper.put('auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', data);
+  return fetchWrapper.put(
+    "auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c",
+    data,
+  );
 }
 
 export async function createAuction(data: FieldValues) {
-    return fetchWrapper.post('auctions', data);
+  return fetchWrapper.post("auctions", data);
 }
 
 export async function getDetailedViewData(id: string): Promise<Auction> {
-    return fetchWrapper.get(`auctions/${id}`);
+  return fetchWrapper.get(`auctions/${id}`);
 }
 
 export async function updateAuction(data: FieldValues, id: string) {
-    return fetchWrapper.put(`auctions/${id}`, data);
+  return fetchWrapper.put(`auctions/${id}`, data);
 }
 
 export async function deleteAuction(id: string) {
-    return fetchWrapper.del(`auctions/${id}`);
+  return fetchWrapper.del(`auctions/${id}`);
+}
+
+export async function getBidsForAuction(id: string): Promise<Bid[]> {
+  return fetchWrapper.get(`bids/${id}`);
 }
--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,4 +1,7 @@
-import { getDetailedViewData } from "@/app/actions/auctionActions";
+import {
+  getBidsForAuction,
+  getDetailedViewData,
+} from "@/app/actions/auctionActions";
 import Heading from "@/app/components/Heading";
 import CountdownTimer from "../../CountdownTimer";
 import CarImage from "../../CarImage";
@@ -7,45 +10,55 @@ import EditButton from "./EditButton";
 import { getCurrentUser } from "@/app/actions/authActions";
 import DeleteButton from "./DeleteButton";
 
-export default async function Details({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
-    const data = await getDetailedViewData(id);
-    const user = await getCurrentUser();
+export default async function Details({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const data = await getDetailedViewData(id);
+  const user = await getCurrentUser();
+  const bids = await getBidsForAuction(id);
 
-    return (
-        <>
-            <div className="flex justify-between">
-                <div className="flex items-center gap-3">
-                    <Heading title={`${data.make} ${data.model}`} />
-                    {user?.username === data.seller && (
-                        <>
-                            <EditButton id={data.id} />
-                            <DeleteButton id={data.id} />
-                        </>
+  return (
+    <>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-3">
+          <Heading title={`${data.make} ${data.model}`} />
+          {user?.username === data.seller && (
+            <>
+              <EditButton id={data.id} />
+              <DeleteButton id={data.id} />
+            </>
+          )}
+        </div>
 
-                    )}
-                </div>
+        <div className="flex gap-3">
+          <h3 className="text-2xl font-semibold">Time remaining:</h3>
+          <CountdownTimer auctionEnd={data.auctionEnd} />
+        </div>
+      </div>
 
-                <div className="flex gap-3">
-                    <h3 className="text-2xl font-semibold">Time remaining:</h3>
-                    <CountdownTimer auctionEnd={data.auctionEnd} />
-                </div>
-            </div>
+      <div className="grid grid-cols-2 gap-6 mt-3">
+        <div
+          className="relative w-full bg-gray-200 aspect-[16/10] 
+                    rounded-lg overflow-hidden"
+        >
+          <CarImage imageUrl={data.imageUrl} />
+        </div>
+        <div className="border-2 rounded-lg p-2 bg-gray-200">
+          <Heading title="Bids" />
+          {bids.map((bid) => (
+            <p key={bid.id}>
+              {bid.bidder} - {bid.amount}
+            </p>
+          ))}
+        </div>
+      </div>
 
-            <div className="grid grid-cols-2 gap-6 mt-3">
-                <div className="relative w-full bg-gray-200 aspect-[16/10] 
-                    rounded-lg overflow-hidden">
-                    <CarImage imageUrl={data.imageUrl} />
-                </div>
-                <div className="border-2 rounded-lg p-2 bg-gray-200">
-                    <Heading title="Bids" />
-                </div>
-            </div>
-
-            <div className="mt-3 grid grid-cols-1 rounded-lg">
-                <DetailedSpecs auction={data} />
-            </div>
-        </>
-
-    )
+      <div className="mt-3 grid grid-cols-1 rounded-lg">
+        <DetailedSpecs auction={data} />
+      </div>
+    </>
+  );
 }
