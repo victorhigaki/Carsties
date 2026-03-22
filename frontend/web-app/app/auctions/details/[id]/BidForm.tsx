60e007a99cd62b52ff519b3e9d8c39dbf254e1ef
Victor Higaki <victorhigaki@outlook.com>
2026-03-22 18:19:07 -0300

    164. Updating the error handling

--- frontend/web-app/app/auctions/details/[id]/BidForm.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -4,6 +4,7 @@ import { placeBidForAuction } from "@/app/actions/auctionActions";
 import { useBidStore } from "@/hooks/useBidStore";
 import { numberWithCommas } from "@/lib/numberWithComma";
 import { FieldValues, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 type Props = {
   auctionId: string;
@@ -15,10 +16,16 @@ export default function BidForm({ auctionId, highBid }: Props) {
   const addBid = useBidStore((state) => state.addBid);
 
   function onSubmit(data: FieldValues) {
-    placeBidForAuction(auctionId, +data.amount).then((bid) => {
-      addBid(bid);
-      reset();
-    });
+    placeBidForAuction(auctionId, +data.amount)
+      .then((bid) => {
+        if (bid.error) {
+          reset();
+          throw bid.error;
+        }
+        addBid(bid);
+        reset();
+      })
+      .catch((err) => toast.error(err.message));
   }
 
   return (
--- frontend/web-app/lib/fetchWrapper.ts
+++ frontend/web-app/lib/fetchWrapper.ts
@@ -1,72 +1,79 @@
 import { auth } from "@/auth";
 
-const baseUrl = 'http://localhost:6001/';
+const baseUrl = "http://localhost:6001/";
 
 async function get(url: string) {
-    const requestOptions = {
-        method: 'GET',
-        headers: await getHeaders()
-    };
-    const response = await fetch(baseUrl + url, requestOptions);
-    return handleResponse(response);
+  const requestOptions = {
+    method: "GET",
+    headers: await getHeaders(),
+  };
+  const response = await fetch(baseUrl + url, requestOptions);
+  return handleResponse(response);
 }
 
 async function put(url: string, body: unknown) {
-    const requestOptions = {
-        method: 'PUT',
-        headers: await getHeaders(),
-        body: JSON.stringify(body)
-    };
-    const response = await fetch(baseUrl + url, requestOptions);
-    return handleResponse(response);
+  const requestOptions = {
+    method: "PUT",
+    headers: await getHeaders(),
+    body: JSON.stringify(body),
+  };
+  const response = await fetch(baseUrl + url, requestOptions);
+  return handleResponse(response);
 }
 
 async function post(url: string, body: unknown) {
-    const requestOptions = {
-        method: 'POST',
-        headers: await getHeaders(),
-        body: JSON.stringify(body)
-    };
-    const response = await fetch(baseUrl + url, requestOptions);
-    return handleResponse(response);
+  const requestOptions = {
+    method: "POST",
+    headers: await getHeaders(),
+    body: JSON.stringify(body),
+  };
+  const response = await fetch(baseUrl + url, requestOptions);
+  return handleResponse(response);
 }
 
 async function del(url: string) {
-    const requestOptions = {
-        method: 'DELETE',
-        headers: await getHeaders()
-    };
-    const response = await fetch(baseUrl + url, requestOptions);
-    return handleResponse(response);
+  const requestOptions = {
+    method: "DELETE",
+    headers: await getHeaders(),
+  };
+  const response = await fetch(baseUrl + url, requestOptions);
+  return handleResponse(response);
 }
 
 async function handleResponse(response: Response) {
-    const text = await response.text();
-    const data = text && JSON.parse(text);
-    if (response.ok) {
-        return data || response.statusText;
-    } else {
-        const error = {
-            status: response.status,
-            message: response.statusText
-        }
-        return {error}
-    }
+  const text = await response.text();
+  let data;
+
+  try {
+    data = text ? JSON.parse(text) : null;
+  } catch {
+    data = text;
+  }
+
+  if (response.ok) {
+    return data || response.statusText;
+  } else {
+    const error = {
+      status: response.status,
+      message: typeof data === "string" ? data : response.statusText,
+    };
+    return { error };
+  }
 }
 
 async function getHeaders(): Promise<Headers> {
-    const session = await auth();
-    const headers = new Headers();
-    headers.set('Content-type', 'application/json');
-    if (session) {
-        headers.set('Authorization', 'Bearer ' + session.accessToken)
-    }
-    return headers;
+  const session = await auth();
+  const headers = new Headers();
+  headers.set("Content-type", "application/json");
+  if (session) {
+    headers.set("Authorization", "Bearer " + session.accessToken);
+  }
+  return headers;
 }
 
 export const fetchWrapper = {
-    get,
-    post,
-    put,
-    del
-}
+  get,
+  post,
+  put,
+  del,
+};
